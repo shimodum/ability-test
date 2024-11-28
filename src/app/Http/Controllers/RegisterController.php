0d02c3462cb6bcf8666dd4e86a2993188f57c82e
akihiro shimoduma <a.shimodum@gmail.com>
2024-11-28 22:03:24 +0900

    ログインページ作成

---
+++ src/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(RegisterRequest $request)
+    {
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return redirect()->route('login');
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,13 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\RegisterController;
 
 
 
 
-Route::get('/', [ContactController::class, 'index']);
-Route::post('/confirm', [ContactController::class, 'confirm']);
-Route::post('/store', [ContactController::class, 'store']);
-Route::get('/thanks', [ContactController::class, 'thanks']);
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::get('/admin', function () {
+    return '管理画面';
+})->name('admin')->middleware('auth');
