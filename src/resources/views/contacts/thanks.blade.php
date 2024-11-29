84a280336f752c74772b56fb6abf081c79b16ca3
akihiro shimoduma <a.shimodum@gmail.com>
2024-11-29 13:43:42 +0900

    お問い合わせフォーム画面遷移OK

--- src/resources/views/contacts/thanks.blade.php
+++ src/resources/views/contacts/thanks.blade.php
@@ -10,7 +10,21 @@
 <body>
     <h1>Thank you</h1>
     <p>お問い合わせありがとうございました</p>
-    <a href="/">HOME</a>
+    <form action="{{ route('contacts.index') }}" method="get" style="display: inline;">
+    <button type="submit" style="
+        padding: 10px 20px;
+        background-color: #8b7268;
+        color: white;
+        font-size: 14px;
+        text-align: center;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    ">
+        HOME
+    </button>
+    </form>
+
 </body>
 
 </html>
