21044c17ee6888245e51be7f1719f31170be2a7b
akihiro shimoduma <a.shimodum@gmail.com>
2024-12-02 00:17:39 +0900

    contactsテーブルのカラム修正

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -44,8 +44,8 @@ class ContactRequest extends FormRequest
             'tel3.digits' => '電話番号は5桁までの数字で入力してください',
             'address.required' => '住所を入力してください',
             'category_id.required' => 'お問い合わせの種類を選択してください',
-            'content.required' => 'お問い合わせ内容を入力してください',
-            'content.max' => 'お問い合わせ内容は120文字以内で入力してください',
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.max' => 'お問い合わせ内容は120文字以内で入力してください',
         ];
     }
 
